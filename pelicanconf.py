03e4ff73809a1a9a2669ba508f9c18951f43ca37
Jonas Teuwen <jonasteuwen@gmail.com>
2021-05-02 21:28:38 +0200

    First commit

---
+++ pelicanconf.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+AUTHOR = "Jonas Teuwen"
+SITENAME = "AI for Oncology"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Europe/Amsterdam"
+
+DEFAULT_LANG = "en"
+DATE_FORMATS = {"en": "%b %d, %Y"}
+
+# Plugins and extensions
+MARKDOWN = {
+    "extension_configs": {
+        "markdown.extensions.admonition": {},
+        "markdown.extensions.codehilite": {"css_class": "highlight"},
+        "markdown.extensions.extra": {},
+        "markdown.extensions.meta": {},
+        "markdown.extensions.toc": {"permalink": " "},
+    }
+}
+
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = [
+    "extract_toc",
+    "liquid_tags.img",
+    "liquid_tags.include_code",
+    "neighbors",
+    "related_posts",
+    "render_math",
+    "series",
+    "share_post",
+    # "tipue_search",
+]
+SITEMAP = {
+    "format": "xml",
+    "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.5},
+    "changefreqs": {"articles": "monthly", "indexes": "daily", "pages": "monthly"},
+}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
